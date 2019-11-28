ecc488dc1c05a99e944a9f29f9a31638739490ef
JunquerGJ <JunquerG@localhost.com>
2019-11-28 17:05:04 +0100

    Changes on methodology entity

--- src/routes/methodologies/methodology.entity.ts
+++ src/routes/methodologies/methodology.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column,PrimaryGeneratedColumn, OneToMany, ManyToOne } from "typeorm";
+import { Entity, Column,PrimaryGeneratedColumn, OneToMany, ManyToOne, Index } from "typeorm";
 import { IsDefined, IsString, IsNotEmpty, ValidateNested, IsArray } from "class-validator";
 import Test from "../tests/test.entity";
 import Company from "../companies/company.entity";
@@ -8,21 +8,18 @@ import { Type } from "class-transformer";
 
 
 @Entity()
+@Index(["name","company"],{ unique : true})
 class Methodology {
     @IsDefined()
     @PrimaryGeneratedColumn()
     id!: number;
 
     @IsString()
-    @Column("varchar",{ length : 15 })
+    @Column("varchar",{ length : 50 })
     name: string;
 
-    @IsArray()
-    @ValidateNested({
-        each : true
-    })
     @Type(() => Test)
-    @OneToMany(type => Test, (test: Test) => test.methodology)
+    @OneToMany(type => Test, (test: Test) => test.methodology,{ cascade : ["insert"]})
     public tests: Test[]
 
     @ValidateNested()
