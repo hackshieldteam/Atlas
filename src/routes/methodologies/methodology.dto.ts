f38e8fc9c76301c8458048c7b236ad22a41b6c58
JunquerGJ <JunquerG@localhost.com>
2019-11-28 10:41:46 +0100

    Added DTOs for methodologies

---
+++ src/routes/methodologies/methodology.dto.ts
@@ -0,0 +1,41 @@
+import { IsDate, IsDefined, IsString, ValidateNested, IsOptional, IsArray } from "class-validator";
+import { Type } from "class-transformer";
+import Company from "../companies/company.entity";
+import Test from "../tests/test.entity";
+
+class CreateMethodologyDto{
+    @IsDefined()
+    @IsString()
+    public name : string
+
+
+    @IsDefined()
+    @Type(() => Company)
+    @ValidateNested()
+    public company: Company;
+
+    @ValidateNested({
+        each : true
+    })
+    @Type(() => Test)
+    tests : Test[]
+}
+
+class UpdateMethodologyDto{
+    @IsOptional()
+    @IsString()
+    public name : string
+}
+
+
+class FindMethodologyDto{
+    @IsOptional()
+    @IsString()
+    public name : string
+}
+
+export{
+    UpdateMethodologyDto,
+    CreateMethodologyDto,
+    FindMethodologyDto
+}
